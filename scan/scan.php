c46ce25384b649c6614c035f98b0ff2da9d10481
Riccardo Marotti <riccardo@marotti.xyz>
2017-02-22 22:19:32 +0100

    Add minimum check url logic

--- scan/scan.php
+++ scan/scan.php
@@ -1,2 +1,12 @@
 <?php
 require 'vendor/autoload.php';
+
+$client = new \GuzzleHttp\Client();
+$csv = \League\Csv\Reader::createFromPath($argv[1]);
+foreach($csv as $csvRow) {
+    try {
+        $httpResponse = $client->get($csvRow[0]);
+    } catch(\Exception $e) {
+	echo $csvRow[0] . PHP_EOL;
+    }
+}
